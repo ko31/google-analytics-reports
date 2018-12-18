132a5399aba3f2f1f1acb716b98a0cd2a6528b37
Ko Takagi <knockoutmarch@gmail.com>
2018-12-18 16:21:46 +0900

    Add error handling

--- src/GoogleAnalyticsReports/Analytics.php
+++ src/GoogleAnalyticsReports/Analytics.php
@@ -112,12 +112,12 @@ final class Analytics {
 	 *
 	 * @param array $args
 	 *
-	 * @return The Analytics Reporting API V4 response.
+	 * @return The Analytics Reporting API V4 response.|WP_Error
 	 */
 	function get_report( $args = [] ) {
 
 		if ( empty( $this->analytics ) ) {
-			return new \WP_Error( 500, __( 'API connection is invalid', $this->prefix ) );
+			return;
 		}
 
 		$defaults = [
@@ -139,80 +139,94 @@ final class Analytics {
 			return $result;
 		}
 
-		// Create the DateRange object.
-		$dateRange = new \Google_Service_AnalyticsReporting_DateRange();
-		$dateRange->setStartDate( $args['from'] );
-		$dateRange->setEndDate( $args['to'] );
+		try {
 
-		// Create the Metrics object.
-		$metrics = [];
-		if ( ! is_array( $args['metrics'] ) ) {
-			$args['metrics'] = (array) $args['metrics'];
+			// Create the DateRange object.
+			$dateRange = new \Google_Service_AnalyticsReporting_DateRange();
+			$dateRange->setStartDate( $args['from'] );
+			$dateRange->setEndDate( $args['to'] );
+
+			// Create the Metrics object.
+			$metrics = [];
+			if ( ! is_array( $args['metrics'] ) ) {
+				$args['metrics'] = (array) $args['metrics'];
+			}
+			array_map( function ( $metric ) use ( &$metrics ) {
+				$_m = new \Google_Service_AnalyticsReporting_Metric();
+				$_m->setExpression( $metric );
+				$_m->setAlias( str_replace( 'ga:', '', $metric ) );
+				$metrics[] = $_m;
+			}, $args['metrics'] );
+
+			//Create the Dimensions object.
+			$dimensions = [];
+			if ( ! is_array( $args['dimensions'] ) ) {
+				$args['dimensions'] = (array) $args['dimensions'];
+			}
+			array_map( function ( $dimension ) use ( &$dimensions ) {
+				$_d = new \Google_Service_AnalyticsReporting_Dimension();
+				$_d->setName( $dimension );
+				$dimensions[] = $_d;
+			}, $args['dimensions'] );
+
+			// Create the Orderby object.
+			$orderby = new \Google_Service_AnalyticsReporting_OrderBy();
+			$orderby->setFieldName( $args['sortFieldName'] );
+			$orderby->setOrderType( $args['sortOrderType'] );
+			$orderby->setSortOrder( $args['sortOrder'] );
+
+			// Create the ReportRequest object.
+			$request = new \Google_Service_AnalyticsReporting_ReportRequest();
+			$request->setViewId( $this->view_id );
+			$request->setDateRanges( [ $dateRange ] );
+			$request->setDimensions( [ $dimensions ] );
+			$request->setMetrics( [ $metrics ] );
+			$request->setOrderBys( [ $orderby ] );
+			$request->setPageSize( $args['pageSize'] );
+
+			if ( ! empty( $args['dimensionFilter'] ) ) {
+				// Create the DimensionFilter object.
+				$segmentDimensionFilter = new \Google_Service_AnalyticsReporting_SegmentDimensionFilter;
+				$segmentDimensionFilter->setDimensionName( $args['dimensionFilter']['dimensionName'] );
+				$segmentDimensionFilter->setOperator( $args['dimensionFilter']['operator'] );
+				$segmentDimensionFilter->setExpressions( $args['dimensionFilter']['expressions'] );
+
+				$dimensionFilterClause = new \Google_Service_AnalyticsReporting_DimensionFilterClause;
+				$dimensionFilterClause->setFilters( $segmentDimensionFilter );
+
+				$request->setDimensionFilterClauses( $dimensionFilterClause );
+			}
+
+			// Call the batchGet method.
+			$body = new \Google_Service_AnalyticsReporting_GetReportsRequest();
+			$body->setReportRequests( [ $request ] );
+
+			$result = $this->analytics->reports->batchGet( $body );
+
+			delete_transient( $transient_key );
+			/**
+			 * gar_set_transient_expiration
+			 *
+			 * @param int $expiration
+			 *
+			 * @return int
+			 */
+			$expiration = apply_filters( 'gar_set_transient_expiration', $this->transient_expiration );
+			set_transient( $transient_key, $result, $expiration );
+
+			return $result;
+
+		} catch ( \Exception $e ) {
+
+			$result = json_decode( $e->getMessage() );
+			if ( json_last_error() === JSON_ERROR_NONE ) {
+				return new \WP_Error( 500, sprintf( __( 'API request failed (%s %s)', $this->prefix ), $result->error->code, $result->error->message ) );
+			}
+
+			return new \WP_Error( 500, __( 'API request failed', $this->prefix ) );
 		}
-		array_map( function ( $metric ) use ( &$metrics ) {
-			$_m = new \Google_Service_AnalyticsReporting_Metric();
-			$_m->setExpression( $metric );
-			$_m->setAlias( str_replace( 'ga:', '', $metric ) );
-			$metrics[] = $_m;
-		}, $args['metrics'] );
 
-		//Create the Dimensions object.
-		$dimensions = [];
-		if ( ! is_array( $args['dimensions'] ) ) {
-			$args['dimensions'] = (array) $args['dimensions'];
-		}
-		array_map( function ( $dimension ) use ( &$dimensions ) {
-			$_d = new \Google_Service_AnalyticsReporting_Dimension();
-			$_d->setName( $dimension );
-			$dimensions[] = $_d;
-		}, $args['dimensions'] );
 
-		// Create the Orderby object.
-		$orderby = new \Google_Service_AnalyticsReporting_OrderBy();
-		$orderby->setFieldName( $args['sortFieldName'] );
-		$orderby->setOrderType( $args['sortOrderType'] );
-		$orderby->setSortOrder( $args['sortOrder'] );
-
-		// Create the ReportRequest object.
-		$request = new \Google_Service_AnalyticsReporting_ReportRequest();
-		$request->setViewId( $this->view_id );
-		$request->setDateRanges( [ $dateRange ] );
-		$request->setDimensions( [ $dimensions ] );
-		$request->setMetrics( [ $metrics ] );
-		$request->setOrderBys( [ $orderby ] );
-		$request->setPageSize( $args['pageSize'] );
-
-		if ( ! empty( $args['dimensionFilter'] ) ) {
-			// Create the DimensionFilter object.
-			$segmentDimensionFilter = new \Google_Service_AnalyticsReporting_SegmentDimensionFilter;
-			$segmentDimensionFilter->setDimensionName( $args['dimensionFilter']['dimensionName'] );
-			$segmentDimensionFilter->setOperator( $args['dimensionFilter']['operator'] );
-			$segmentDimensionFilter->setExpressions( $args['dimensionFilter']['expressions'] );
-
-			$dimensionFilterClause = new \Google_Service_AnalyticsReporting_DimensionFilterClause;
-			$dimensionFilterClause->setFilters( $segmentDimensionFilter );
-
-			$request->setDimensionFilterClauses( $dimensionFilterClause );
-		}
-
-		// Call the batchGet method.
-		$body = new \Google_Service_AnalyticsReporting_GetReportsRequest();
-		$body->setReportRequests( [ $request ] );
-
-		$result = $this->analytics->reports->batchGet( $body );
-
-		delete_transient( $transient_key );
-		/**
-		 * gar_set_transient_expiration
-		 *
-		 * @param int $expiration
-		 *
-		 * @return int
-		 */
-		$expiration = apply_filters( 'gar_set_transient_expiration', $this->transient_expiration );
-		set_transient( $transient_key, $result, $expiration );
-
-		return $result;
 	}
 
 }
