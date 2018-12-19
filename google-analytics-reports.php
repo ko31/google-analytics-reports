8ec1bf5f5f2158d18931073c5a5d364b13b83a34
Ko Takagi <knockoutmarch@gmail.com>
2018-12-20 00:16:08 +0900

    Update comment

--- google-analytics-reports.php
+++ google-analytics-reports.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Google Analytics Reports
- * Plugin URI:      https://github.com/ko31/google-analytics-reports
- * Description:
- * Author:          ko31
- * Author URI:      https://go-sign.info
+ * Plugin URI:      https://github.com/tarosky/google-analytics-reports
+ * Description:     This is plugin to retreive data from Google Analytics Reporting API.
+ * Author:          tarosky, ko31
+ * Author URI:      https://tarosky.co.jp
  * Text Domain:     google-analytics-reports
  * Domain Path:     /languages
  * Version:         1.0.0
