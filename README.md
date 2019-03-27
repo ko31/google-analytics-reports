fd2e301fc2c60b59b8d4b1e9fd7c15dfde5bc364
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-03-27 22:11:45 +0900

    automated release and update

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Google Analytics Reports
 
-Contributors: ko31,tarosky  
-Tags:  
-Requires at least: 4.5  
-Tested up to: 5.1  
-Requires PHP: 5.6  
-Stable tag: 1.0.0  
-License: GPLv2 or later  
+Contributors: ko31,tarosky
+Tags:
+Requires at least: 4.5
+Tested up to: 5.1
+Requires PHP: 5.6
+Stable tag: 1.0.0
+License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 This is Google Analytics Reports.
@@ -15,3 +15,15 @@ This is Google Analytics Reports.
 
 This is Google Analytics Reports.
 
+## How to release
+
+Tag as version number.
+
+```
+$ git tag 1.0.1
+$ git push origin 1.0.1
+```
+
+Then you can see new version in following after couple of minutes.
+
+https://github.com/tarosky/google-analytics-reports/releases
--- google-analytics-reports.php
+++ google-analytics-reports.php
@@ -7,11 +7,21 @@
  * Author URI:      https://tarosky.co.jp
  * Text Domain:     google-analytics-reports
  * Domain Path:     /languages
- * Version:         1.0.0
+ * Version:         nightly
  *
  * @package         GoogleAnalyticsReports
  */
 
 require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
+
+add_action( 'init', 'google_analytics_reports_activate_autoupdate' );
+
+function google_analytics_reports_activate_autoupdate() {
+	$plugin_slug = plugin_basename( __FILE__ ); // e.g. `hello/hello.php`.
+	$gh_user = 'tarosky';                      // The user name of GitHub.
+	$gh_repo = 'google-analytics-reports';       // The repository name of your plugin.
+	// Activate automatic update.
+	new GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
+}
 
 Tarosky\GoogleAnalyticsReports::get_instance()->register();
